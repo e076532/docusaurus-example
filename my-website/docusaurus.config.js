454ecb1bd7cd8556198bdc92e336be06a3c9d71e
ezhang <ezhang@EC2AMAZ-SK4AVLI>
2022-02-08 11:11:13 -0800

    add trailingSlash

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'e076532', // Usually your GitHub org/user name.
   projectName: 'docusaurus-example', // Usually your repo name.
+  trailingSlash: false,
 
   presets: [
     [
