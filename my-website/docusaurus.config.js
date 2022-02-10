4f47f40c0e03f669974a25f384b3577876dae086
ezhang <ezhang@EC2AMAZ-SK4AVLI>
2022-02-10 09:45:19 -0800

    modify the url

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -23,9 +23,10 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+		  //routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/e076532/docusaurus-example/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
