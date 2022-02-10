37271e8e5a11bf283cf8cac973a61976c1d20ede
ezhang <ezhang@EC2AMAZ-SK4AVLI>
2022-02-10 15:49:19 -0800

    modify the docs

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -6,8 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'mastercard',
   url: 'https://e076532.github.io',
   baseUrl: '/docusaurus-example/',
   onBrokenLinks: 'throw',
